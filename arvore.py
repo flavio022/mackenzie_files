42d07795a86597a9126ff5a0565ba1233e5f250b
flaviodcosta <flavio.costa@beneficiofacil.com.br>
2017-10-23 18:58:56 -0200

    creating a class of tree

---
+++ arvore.py
@@ -0,0 +1,26 @@
+class ArvoreBinBusca:
+    class __no:
+        def __init(self,e):
+            self.item =e
+            self.pai = None
+            self.direita = None
+            self.esquerda = None
+
+            def getItem(self):
+                return self.item
+            def getPai(self):
+                return self.pai
+            def getDireita(self):
+                return self.direita
+            def getEsquerda(self):
+                return self.esquerda
+            def setItem(self,e):
+                self.item = e
+            def setPai(self,e):
+                self.pai = e
+            def setEsquerda(self,e):
+                self.esquerda = e
+            def setDireita(self,e):
+                self.direita = e
+    def __init(self):
+        self.raiz = None
